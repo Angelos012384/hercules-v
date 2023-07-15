8e66ce5d0a4b97063a94bc68fcd20dd6afddb6e6
Angelos012384 <79968091+Angelos012384@users.noreply.github.com>
2023-07-15 18:41:09 +0300

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,20 @@
+function BlueStar1(){
+    var imo = "9197105";
+    var sType = "Ro-Ro/Passenger Ship";
+
+    imoData = document.querySelector(".bs1");
+    typeData = document.querySelector(".bs1-2");
+
+    imoData.innerHTML = "IMO: "+imo;
+    typeData.innerHTML = "Type: "+sType;
+}
+function Patmos(){
+    var imo = "9565041";
+    var sType = "Ro-Ro/Passenger Ship";
+
+    imoData = document.querySelector(".bsP1");
+    typeData = document.querySelector(".bsP1-2");
+
+    imoData.innerHTML = "IMO: "+imo;
+    typeData.innerHTML = "Type: "+sType;
+}
